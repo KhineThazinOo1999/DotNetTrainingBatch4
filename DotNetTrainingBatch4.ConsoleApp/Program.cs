0e12e5d224085f2b89623ac129e906e2c7c1e632
KhineThazinOo1999 <khinethazinoo.6899@gmail.com>
2024-05-13 18:22:41 +0630

    [ADD] Dapper CRUD

--- DotNetTrainingBatch4.ConsoleApp/Program.cs
+++ DotNetTrainingBatch4.ConsoleApp/Program.cs
@@ -47,11 +47,19 @@ Console.WriteLine("Hello, World!");
 //CRUD 
 
 
-AdoDotNetExample adoDotNetExample = new AdoDotNetExample();
+//AdoDotNetExample adoDotNetExample = new AdoDotNetExample();
 //adoDotNetExample.Read();
 // adoDotNetExample.Create("title","author","content");
 //adoDotNetExample.Update(11, "test title", "test author", "test content");
 //adoDotNetExample.Delete(11);
-adoDotNetExample.Edit(1002);
-adoDotNetExample.Edit(1);
+//adoDotNetExample.Edit(1002);
+//adoDotNetExample.Edit(1);
+
+
+
+DapperExample dapperExample = new DapperExample();
+dapperExample.Run();
+
+
+
 Console.ReadKey();
---
+++ README.md
@@ -0,0 +1,3 @@
+2024-04-09 Console App
+2024-04-10 Ado.Net
+2024-04-22 Dapper (ORM - relation between DB and C#)
